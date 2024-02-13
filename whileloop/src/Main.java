9dee2ac283ee6ad7df3a9abc6b2ebf5b5485b56c
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-13 16:25:52 +0530

    Add files via upload

---
+++ whileloop/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no: ");
+        int n = sc.nextInt();
+
+        while (n <= 10){
+            System.out.println(n);
+            n++;
+
+
+        }
+
+    }
+}
